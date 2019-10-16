0c0d0999bebb76b6bf7c2150a5e7d423313cf8cd
Ncker2 <56551118+Ncker2@users.noreply.github.com>
2019-10-16 10:34:08 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,12 @@
+/* A set of functions to change the background to tomato (ha-ha!)*/
+function setRed() {
+    document.body.style.background= "red";
+
+}
+ 
+/* Set of functions to change background to white */
+ function setwhite() {
+	document.body.style.background= "white";
+	
+} 
+   
